f632552539100d7eb97165a38c8290f60cae9221
elayapanneer2020 <113533794+elayapanneer2020@users.noreply.github.com>
2022-09-15 21:12:02 +0530

    Add files via upload

---
+++ Arethemetic.java
@@ -0,0 +1,23 @@
+package bharu;
+
+public class Arethemetic {
+
+	public static void main(String[] args) {
+		int num1, num2, ans;
+		num1=20;
+		num2=10;
+		//multiplication
+		ans=num1*num2;
+		System.out.println("mul"+ans);
+		
+		//division
+		ans=num1/num2;
+		System.out.println("div"+ans);
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
